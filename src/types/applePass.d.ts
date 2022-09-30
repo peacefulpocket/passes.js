a3b846bfd93c7997a75f926aa286aa750776558b
Lucy Dunham <lucy@lucy.link>
2022-09-30 13:39:00 +1000

    add util file with rgb type

---
+++ src/types/applePass.d.ts
@@ -0,0 +1,76 @@
+import { RGB } from './util';
+
+export type AppleWalletPassFieldContent = {
+  key: string,
+  value: string | Date | number,
+  label: string,
+  changeMessage: string,
+  attributedValue: string | Date | number,
+  currency: string,
+  dataDetectorTypes: string[],
+  dateStyle: 'PKDateStyleNone', 'PKDateStyleShort', 'PKDateStyleMedium', 'PKDateStyleLong', 'PKDateStyleFull',
+  ignoreTimeZone: boolean,
+  relativeDate: boolean,
+  numberStyle: 'PKNumberStyleDecimal' | 'PKNumberStylePercent' | 'PKNumberStyleScientific' | 'PKNumberStyleSpellOut',
+  textAlignment: 'PKTextAlignmentLeft' | 'PKTextAlignmentCenter' | 'PKTextAlignmentRight' | 'PKTextAlignmentNatural',
+  timeStyle: 'PKDateStyleNone', 'PKDateStyleShort', 'PKDateStyleMedium', 'PKDateStyleLong', 'PKDateStyleFull'
+};
+
+export type AppleWalletAuxField = AppleWalletPassFieldContent & { row: number };
+
+export type AppleWalletPassFields = {
+  auxFields: AppleWalletAuxField[],
+  backFields: AppleWalletPassFieldContent[],
+  HeaderFields: AppleWalletPassFieldContent[],
+  primaryFields: AppleWalletPassFieldContent[],
+  secondaryFields: AppleWalletPassFieldContent[],
+};
+
+export type AppleWalletBarcode = {
+  message: string,
+  format: 'PKBarcodeFormatQR' | 'PKBarcodeFormatPDF417' | 'PKBarcodeFormatAztec' | 'PKBarcodeFormatCode128',
+  messageEncoding: string,
+  altText: string,
+};
+
+export type AppleWalletBeacon = {
+  proximityUUID: string,
+  relevantText: string,
+  major: number,
+  minor: number,
+};
+
+export type AppleWalletLocation = {
+  latitude: number,
+  longitude: number,
+  altitude: number,
+  relevantText: string,
+};
+
+export type AppleWalletPass = {
+  passTypeId: string,
+  serialNumber: string,
+  teamId: string,
+  description: string,
+  barcode: AppleWalletBarcode[],
+  beacons: AppleWalletBeacon[],
+  locations: AppleWalletLocation[],
+  maxDistance: number,
+  orgName: string,
+  description: string,
+  bgColour: RGB,
+  fgColour: RGB,
+  labelColour: RGB,
+  sharingProhib: boolean,
+  stripShine: boolean,
+  void: boolean,
+  appUrl: string,
+  associatedStoreId: number[],
+  webServiceURL: string,
+  webServiceAuth: string,
+  expirationDate: Date,
+  relevantDate: Date,
+  groupId: string,
+  logoText: string,
+  userInfo: object,
+};
