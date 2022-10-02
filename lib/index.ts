78d92d149e9810c210b5eaf94e5f59ac46413964
Lucy Dunham <lucy@lucy.link>
2022-10-02 21:33:05 +1100

    implement basic structure

---
+++ lib/index.ts
@@ -0,0 +1,9 @@
+import { AppleWalletPass } from './types/applePass';
+
+export function AppleWalletCreatePassObject(passInfo: AppleWalletPass) {
+
+}
+
+export function AppleWalletValidatePassObject() {
+
+}
