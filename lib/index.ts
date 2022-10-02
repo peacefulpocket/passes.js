12ea0e5928091b838c0e072dcada11406ebf4690
Lucy Dunham <lucy@lucy.link>
2022-10-02 22:30:02 +1100

    basic generation of pass

--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,16 @@
-import { AppleWalletPass } from './types/applePass';
+import { AppleWalletPassInfo, AppleWalletPassObject } from './types/applePass';
 
-export function AppleWalletCreatePassObject(passInfo: AppleWalletPass) {
-
+export function AppleWalletCreatePassObject(passInfo: AppleWalletPassInfo) {
+  const pass: AppleWalletPassObject = {
+    formatVersion: 1,
+    organizationName: passInfo.orgName,
+    passTypeIdentifier: passInfo.passTypeId,
+    serialNumber: passInfo.serialNumber,
+    teamIdentifier: passInfo.teamId,
+    description: passInfo.description,
+    [passInfo.passType]: passInfo.passInfo,
+  };
+  return pass;
 }
 
 export function AppleWalletValidatePassObject() {
