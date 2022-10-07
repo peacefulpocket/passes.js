53f562c4fcd6ec9f60507a31abccda37f124862c
Lucy Dunham <lucy@lucy.link>
2022-10-07 12:11:52 +1100

    reorder  pass and draw the rest of the owl

--- lib/AppleWallet.ts
+++ lib/AppleWallet.ts
@@ -10,11 +10,28 @@ export default function AppleWalletCreatePassObject(passInfo: AppleWalletPassInf
     description: passInfo.description,
     [passInfo.passType]: passInfo.passInfo,
     barcodes: passInfo.barcode,
-    authenticationToken: passInfo.webServiceAuth,
-    webServiceURL: passInfo.webServiceURL,
+    beacons: passInfo.beacons,
+    locations: passInfo.locations,
+    maxDistance: passInfo.maxDistance,
     backgroundColor: passInfo.bgColor,
     foregroundColor: passInfo.fgColor,
     labelColor: passInfo.labelColor,
+    sharingProhibited: passInfo.sharingProhib,
+    suppressStripShine: passInfo.supressStripShine,
+    voided: passInfo.void,
+    appLaunchURL: passInfo.appUrl,
+    associatedStoreIdentifiers: passInfo.associatedStoreId,
+    webServiceURL: passInfo.webServiceURL,
+    authenticationToken: passInfo.webServiceAuth,
+    expirationDate: passInfo.expirationDate?.toISOString(),
+    relevantDate: passInfo.relevantDate?.toISOString(),
+    groupingIdentifier: passInfo.groupId,
+    logoText: passInfo.logoText,
+    userInfo: passInfo.userInfo,
+    personalize: passInfo.personalize,
+    nfc: passInfo.NFC,
+    semantics: passInfo.semantics,
   };
-  return pass;
+  const filteredPass = Object.fromEntries(Object.entries(pass).filter(([, value]) => !!value));
+  return filteredPass;
 }
