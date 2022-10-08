54917566e133e4a1a40364c76d169687bc3c8858
Lucy Dunham <lucy@lucy.link>
2022-10-08 21:32:18 +1100

    [owl] add function to sign apple manifests

--- lib/AppleWallet.ts
+++ lib/AppleWallet.ts
@@ -54,3 +54,42 @@ export function AppleWalletCreateManifest(folderPath: string) {
   const manifest = manifestArray.reduce((acc, { name, sha1 }) => ({ ...acc, [name]: sha1 }), {});
   return manifest;
 }
+
+export function AppleWalletSignManifest(manifestPath: string, signCertPath: string) {
+  const signBinary = readFileSync(signCertPath, 'binary');
+  const signAsn1 = forge.asn1.fromDer(signBinary);
+  const signP12 = forge.pkcs12.pkcs12FromAsn1(signAsn1);
+  const signKeyBags = signP12.getBags({ bagType: forge.pki.oids.pkcs8ShroudedKeyBag });
+  const signKeyBag = signKeyBags[forge.pki.oids.pkcs8ShroudedKeyBag]![0];
+  const signCertBags = signP12.getBags({ bagType: forge.pki.oids.certBag });
+  const signCertBag = signCertBags[forge.pki.oids.certBag]![0];
+  const signKey = signKeyBag.key!;
+  const signCert = signCertBag.cert!;
+  const wwdrBinary = readFileSync('wwdr/wwdr.cer', 'binary');
+  const wwdrAsn1 = forge.asn1.fromDer(wwdrBinary);
+  const wwdr = forge.pki.certificateFromAsn1(wwdrAsn1);
+  const pkcs7 = forge.pkcs7.createSignedData();
+  pkcs7.content = readFileSync(`${manifestPath}/manifest.json`, 'utf8');
+  pkcs7.addCertificate(signCert);
+  pkcs7.addCertificate(wwdr);
+  pkcs7.addSigner({
+    key: signKey as forge.pki.rsa.PrivateKey,
+    certificate: signCert,
+    digestAlgorithm: forge.pki.oids.sha1,
+    authenticatedAttributes: [
+      {
+        type: forge.pki.oids.contentType,
+        value: forge.pki.oids.data,
+      },
+      {
+        type: forge.pki.oids.messageDigest,
+      },
+      {
+        type: forge.pki.oids.signingTime,
+      },
+    ],
+  });
+  pkcs7.sign({ detached: true });
+  writeFileSync(`${manifestPath}/signature`, Buffer.from(forge.asn1.toDer(pkcs7.toAsn1()).getBytes(), 'binary'));
+  return Buffer.from(forge.asn1.toDer(pkcs7.toAsn1()).getBytes(), 'binary');
+}
